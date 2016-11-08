92b74455333cb8b8f50c61c9647f01ce5f09f1b2
Girish Kumar <girishkumar@tamuall-165-91-49-87.vpn.tamu.edu>
2016-11-08 17:59:13 -0600

    Modified production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'still-hamlet-97281.herokuapp.com'
+  host = 'csce606-ruythons.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
