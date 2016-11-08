cbcdeb3e80eb6485fde2275899053ccee88cc60e
Digveer <digveer@outlook.com>
2016-11-08 01:29:43 +0000

    Mailer settings done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,5 +3,6 @@ class UserMailer < ApplicationMailer
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset| Project Tracker"
+    Rails.logger.debug("My objectmailer: #{@user.inspect}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,30 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+    #ails.logger.debug('My objectcreate inside mail')
 
+  #   config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  # :address              => "smtp.gmail.com",
+  # :port                 => 587,
+  # :user_name            => 'saasapp@digveer.com',
+  # :password             => 'rubathons',
+  # :authentication       => "plain",
+  # :enable_starttls_auto => true
+  # }
+  
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for mailgun
+ActionMailer::Base.smtp_settings = {
+  :port           => 587,
+  :address        => "smtp.mailgun.org",
+  :domain         => ENV['domain'],
+  :user_name      => ENV['username'],
+  :password       => ENV['password'],
+  :authentication => :plain,
+}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
