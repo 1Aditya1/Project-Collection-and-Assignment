0b7e823e115ef18efeb8913012af5de963b2bbae
Girish Kumar <girishkumar@Girishs-MacBook-Pro.local>
2016-11-08 18:38:26 -0600

    Update to billing details/host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'csce606-ruythons.herokuapp.com'
+  host = 'csce606-rubythons.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
